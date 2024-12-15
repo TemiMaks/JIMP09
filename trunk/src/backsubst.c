e4b55a83870c8840ebfc460909f358404d608846
JedJak <01197803@pw.edu.pl>
2024-12-15 12:56:18 +0100

    Dodano sprawny kod funkcji backsubst

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -1,23 +1,33 @@
+#include <stdio.h>
 #include "backsubst.h"
-/**
- * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
- * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
- * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
- */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-                                /**
-                                 * Tutaj należy umieścić właściwą implemntację.
-                                 */
+#include "matrix.h"
 
-                                /* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+int backsubst(Matrix *x, Matrix *A, Matrix *b) {
+    // Sprawdzenie poprawności wejścia
+    if (x == NULL | | A == NULL | | b == NULL) {
+        fprintf(stderr, "Błąd! Jeden z wskaźników (x, A, b) jest NULL.\n");
+        return 1;
+    }
+    if (A->r != A->c | | A->r != b->r | | b->c != 1 | | x->r != A->r | | x->c != 1) {
+        fprintf(stderr, "Błąd! Nieprawidłowe wymiary macierzy.\n");
+        return 1;
+    }
 
-                                int i;
-                                for (i =0; i < x->r; i++) {
-                                                                x->data[i][0] = b->data[i][0];
-                                }
+    // Wykonanie podstawiania wstecznego
+    for (int i = A->r - 1; i >= 0; i--) {
+        // Sprawdzenie, czy element diagonalny jest zerowy
+        if (A->data[i][i] == 0) {
+            fprintf(stderr, "Błąd! Zerowy element diagonalny w wierszu %d.\n", i);
+            return 1;
+        }
 
-                                return 0;
+        // Obliczenie wartości x[i]
+        double sum = 0.0;
+        for (int j = i + 1; j < A->c; j++) {
+            sum += A->data[i][j] * x->data[j][0];
+        }
+        x->data[i][0] = (b->data[i][0] - sum) / A->data[i][i];
+    }
+
+    return 0; // Sukces
 }
-
-
-
